2a95e93f3a81d33397023c50444f70db6d8d1801
Santu <santue.eghosh@gmail.com>
2022-08-08 23:54:09 +0530

    assingmet

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,6 +10,10 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
+router.get('GET /movies', function (req, res){
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
+})
 router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
@@ -34,5 +38,55 @@ router.get('/student-details/:name', function(req, res){
     
     res.send('Dummy response')
 })
+//////////////////////////////// 1 ////////////////////////////
+router.get('/get-movies',function(req , res){
+    
+    let movies1 = ['M.s Dhoni' , '3 Idiot' , 'Zindagi Na Milegi Dobara' , 'Dangal' ]
+   res.send(movies1)
+})
 
+////////////////////// 2 ///////////////////////////////////////////
+
+router.get('/get-movie:name', function(req , res){
+    
+    let movies = ['M.s Dhoni' , '3 Idiot' , 'Zindagi Na Milegi Dobara','Dangal']
+let index = req.params.indexNumber;
+console.log (movies[index])
+      res.send(movies[index])
+})
+
+////////////////// 3///////////////////////////////////////////////
+router.get('/get-moviess:indexNumber', function(req , res){
+    let movies = ['M.s Dhoni' , '3 Idiot' , 'Zindagi Na Milegi Dobara','Dangal']
+    let index = req.params.indexNumber;
+    if(index > moviesName.length){
+        return res.send("movies length is greater then movioe")
+}else{
+    res.send(moviesNumber[index])
+}   
+})
+
+///////////////////// 4 /////////////////////////////////////////////
+router.get('/get-/films',function(req, res){ //student detail api he    
+
+    let moviesName=[ {"id": 1,"name": "3 Idiot"}, 
+ {"id": 2,"name": "Zindagi Na Milegi Dobara"}, 
+ {"id": 3,"name": "Dangal"},
+  {"id": 4,"name": "Finding Nemo"}]
+    res.send(moviesName)
+})
+///////////////////////// 5 ////////////////////////
+router.get('/get-/films/:indexNumber',function(req, res){ //student detail api he    
+
+    let moviesName=[ {"id": 1,"name": "3 Idiot"}, 
+ {"id": 2,"name": "Zindagi Na Milegi Dobara"}, 
+ {"id": 3,"name": "Dangal"},
+  {"id": 4,"name": "Finding Nemo"}]
+    let index = req.params.indexNumber;
+     if(index > moviesName.length){
+        return res.send("no movie exist with this id ")
+     }else{
+     res.send(moviesName[index])
+     }
+})
 module.exports = router;
