82d67c2850b6cb4b865e7362c9d66eaf5b5735b4
Santu <santue.eghosh@gmail.com>
2022-08-25 14:21:52 +0530

    session/middleware-1

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,33 @@
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
-}
+// const mid1= function ( req, res, next) {
+//     req.falana= "hi there. i am adding something new to the req object"
+//     console.log("Hi I am a middleware named Mid1")
+//     next()
+// }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
+// const mid2= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid2")
+//     next()
+// }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
+// const mid3= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid3")
+//     next()
+// }
 
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
-
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+// const mid4= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid4")
+//     next()
+// }
+app.use (
+    function (req, res, next) {
+        let dmy = moment().format('DD-MM-YYYY, HH:mm:ss');
+        let ipAddress = req.ip;
+        let url = req.originalUrl
+        console.log (dmy +" , "+ipAddress+" , "+url);
+        next();
+    }
+// module.exports.mid1= mid1
+// module.exports.mid2= mid2
+// module.exports.mid3= mid3
+// module.exports.mid4= mid4
