9483c57a82f93419dc5a56af4caf2f7302a7588b
Santu <santue.eghosh@gmail.com>
2022-08-31 12:42:36 +0530

    santyuoi

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,62 +1,93 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
+
 const createUser = async function (req, res) {
-  let userBody = req.body;
-  let user = await userModel.create(userBody);
-  res.send({ msg: user });
+  try {
+    let userBody = req.body;
+    if (Object.keys(userBody).length != 0) {
+      let user = await userModel.create(userBody);
+      res.status(201).send({ msg: user });
+    }
+    else res.status(400).send({ msg: "In the body there is no data present" })
+  }
+  catch (err) {
+    res.status(500).send({ msg: err.message });
+
+  }
 }
+
 
 const loginUser = async function (req, res) {
-  let userBody2 = req.body;
-  let user = userBody2.emailId;
-  let pass = userBody2.password;
-  let validemailpass = await userModel.findOne({ emailId: user, password: pass });
-  if (!validemailpass) return res.send({ status: false, msg: "email or password is wrong" });
-  let uniqueid = validemailpass._id;
-  let token1 = jwt.sign({
-    _id: uniqueid,
-    userid: user,
-    password: pass
-  },
-    "Winners-Never-Quit"
-  )
-  res.header({ "x-auth-token": token1 });
-  res.send({ status: true, data: { token: token1 } });
+  try {
+    let userBody2 = req.body;
+    let user = userBody2.emailId;
+    let pass = userBody2.password;
+    let validemailpass = await userModel.findOne({ emailId: user, password: pass });
+    if (!validemailpass) return res.status(400).send({ status: false, msg: "email or password is wrong" });
+    let uniqueid = validemailpass._id;
+    let token1 = jwt.sign({
+      _id: uniqueid,
+      userid: user,
+      password: pass
+    },
+      "Winners-Never-Quit"
+    )
+    res.header({ "x-auth-token": token1 });
+    res.status(200).send({ status: true, data: { token: token1 } });
+  }
+  catch (err) {
+    res.status(500).send({ msg: err.message })
+  }
 }
 
-//""eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzBkMTJhYzY0YzZlYjY0ODI5N2EwZmMiLCJ1c2VyaWQiOiJyYWp1QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoicmFqdTEyMyIsImlhdCI6MTY2MTgwMTM1NH0.Hya0sa62sSWwHYMJxWb6qgRLvlPPEYotc5wfoO6QsgU"
+
+//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzBjYmU3ZjE2ODY1ZTU5OThjMjg1N2MiLCJ1c2VyaWQiOiJhc2RAZ21haWwuY29tIiwicGFzc3dvcmQiOiJhZEAwMCIsImlhdCI6MTY2MTc5NDM1OX0.NW9jfGF7VRaAjKaF4asYn0kkFCEKoTvXIF3QFWDQnMk"
 
 const getUserData = async function (req, res) {
-  let userid = req.params.userId;
-  let validUser = await userModel.findById(userid);
-  if (!validUser) return res.send({ status: false, msg: "User is not present" });
-  return res.send({ status: true, msg: validUser })
-
+  try {
+    let userid = req.params.userId;
+    let validUser = await userModel.findById(userid);
+    if (!validUser) return res.status(400).send({ status: false, msg: "User is not present" });
+    return res.status(200).send({ status: true, msg: validUser })
+  }
+  catch (err) {
+    res.status(500).send({ msg: err.message })
+  }
 }
+
 
 
 const updateUserdata = async function (req, res) {
-  let userid1 = req.params.userId;
-  let User = await userModel.findById(userid1);
-  if (!User)
-    return res.send({ status: false, msg: "User is not present" });
+  try {
+    let userid1 = req.params.userId;
+    let User = await userModel.findById(userid1);
+    if (!User)
+      return res.status(400).send({ status: false, msg: "User is not present" });
 
 
-  let userData = req.body
-  let updateUserData = await userModel.findOneAndUpdate({ _id: userid1 }, userData, { new: true });
-  res.send({ status: true, msg: updateUserData });
+    let userData = req.body
+    let updateUserData = await userModel.findOneAndUpdate({ _id: userid1 }, userData, { new: true });
+    res.status(200).send({ status: true, msg: updateUserData });
+  }
+  catch (err) {
+    res.status(500).send({ msg: err.message })
+  }
 
 }
 
-const deleteUser = async function(req,res){
-  let delUserid = req.params.userId;
-  let User3 = await userModel.findById(delUserid);
-  if (!User3 ) return res.send({ status: false, msg: "User is not present" });
-  
-  let deleteUserData = await userModel.findOneAndUpdate({ _id: delUserid }, {isDeleted : true}, { new: true });
-  res.send({ status: true, msg: deleteUserData });
+const deleteUser = async function (req, res) {
+  try {
+    let delUserid = req.params.userId;
+    let User3 = await userModel.findById(delUserid);
+    if (!User3) return res.status(400).send({ status: false, msg: "User is not present" });
 
+    let deleteUserData = await userModel.findOneAndUpdate({ _id: delUserid }, { isDeleted: true }, { new: true });
+    res.send.status(200)({ status: true, msg: deleteUserData });
+  }
+  catch (err) {
+    res.status(500).send({ msg: err.message })
+  }
 }
 
 
--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -2,18 +2,43 @@
 const jwt = require("jsonwebtoken")
 
 
+// const isFreeMid = async function (req, res, next) {
+//     let head = req.headers["x-Auth-Token"];
+//     if (!head) 
+//     head = req.headers["x-auth-token"]
+//     if (!head) return res.send({ status: false, msg: "token is not present" });
+//     console.log(head);
+
+//     let validtoken = jwt.verify(head, "Winners-Never-Quit");
+//     if (!validtoken) return res.send({ status: false, msg: "the token is not valid" });
+//     next();
+
+// }
+
+
+// module.exports.isFreeMid = 
 const isFreeMid = async function (req, res, next) {
-    let head = req.headers["x-Auth-Token"];
-    if (!head) 
-    head = req.headers["x-auth-token"]
-    if (!head) return res.send({ status: false, msg: "token is not present" });
-    console.log(head);
+    try {
+        let head = req.headers["x-Auth-Token"];
+        if (!head) head = req.headers["x-auth-token"]
+        if (!head) return res.status(401).send({ status: false, msg: "plss input the token" });
+        console.log(head);
 
-    let validtoken = jwt.verify(head, "Winners-Never-Quit");
-    if (!validtoken) return res.send({ status: false, msg: "the token is not valid" });
-    next();
-
+        let validtoken = jwt.verify(head, "Winners-Never-Quit");
+        if (!validtoken) {
+            return res.status(403).send({ msg: "token is invalid" });
+        }
+        req.loggedinuser = validtoken["_id"];
+        let headid = req.params.userId;
+        if (headid !== req.loggedinuser) {
+            return res.status(404).send({ msg: "Access denied" });
+        }
+        next();
+    }
+    catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
 }
 
-
-module.exports.isFreeMid = isFreeMid
+module.exports.isFreeMid = isFreeMid;
